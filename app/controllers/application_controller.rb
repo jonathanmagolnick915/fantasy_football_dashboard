a1448bbad3a1e8468f82ade2e6d1314a17bfe522
Jonathan Magolnick <jonathanmagolnick@gmail.com>
2017-10-31 23:38:12 -0400

    nav-bar and tables

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_layout_variables
+
+  def set_layout_variables
+    @all_teams = Team.all
+    @league_name = Yahoo.get_league_metadata[:league_name]
+  end
 end
